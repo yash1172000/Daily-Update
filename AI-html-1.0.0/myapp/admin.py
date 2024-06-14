27e0a27d1abb285cb265711e4a5bae08281db548
Yash Patel <yash.p@borntechies.com>
2024-06-14 12:16:39 +0530

    2024-14-june

--- AI-html-1.0.0/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User,WorkSpace,Uploadfiles,Onboard,Admin_user,Pricing,Feedback
-admin.site.register(User)
-admin.site.register(WorkSpace)
-admin.site.register(Feedback)
-admin.site.register(Uploadfiles)
-admin.site.register(Onboard)
-admin.site.register(Admin_user)
-admin.site.register(Pricing)
